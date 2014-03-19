e856cd46f9f95a08da12fb0e1a9d8c2436679370
marushkevych <marushkevych@gmail.com>
2014-03-19 08:12:52 -0400

    get comments cross domain

--- server.js
+++ server.js
@@ -107,11 +107,11 @@ var SampleApp = function() {
         
         self.routes['/comments'] = function(req, res) {
             res.setHeader('Content-Type', 'application/json');
-            res.setHeader('Access-Control-Allow-Credentials', true);
+            res.setHeader('Access-Control-Allow-Credentials', 'true');
             res.setHeader('Access-Control-Allow-Origin', '*');
             res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
             res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-            res.send("{'name':'bla', 'needRide':false}");
+            res.send("{}");
         };
     };
 
